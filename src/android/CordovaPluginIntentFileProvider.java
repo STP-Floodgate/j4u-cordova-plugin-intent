15a179fad51871240fc47f3add76a75dc592193f
Ciaran Hikaru Ueda Fitzgerald <43626217+CHUF04@users.noreply.github.com>
2024-12-25 16:26:17 +0900

    references changed to j4u

--- src/android/CordovaPluginIntentFileProvider.java
+++ src/android/CordovaPluginIntentFileProvider.java
@@ -1,4 +1,4 @@
-package com.darryncampbell.cordova.plugin.intent;
+package org.japanforunhcr.cordova.plugin.intent;
 
 public class CordovaPluginIntentFileProvider extends android.support.v4.content.FileProvider {
 }
